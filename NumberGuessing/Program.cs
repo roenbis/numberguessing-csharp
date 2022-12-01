6041360a9f32ce85c56f6c3bd273103314ec9c83
roenbis <bekzat.bagdatuly03@gmail.com>
2022-12-01 10:29:56 +0600

    numberguess

---
+++ NumberGuessing/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace NumberGuessing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+
+            int winNum = r.Next(0, 100);
+
+            bool win = false;
+
+            do
+            {
+                Console.Write("Guess A Number In-between 0 And 100: ");
+                string s = Console.ReadLine();
+                int i = int.Parse(s);
+                if (i > winNum)
+                {
+                    Console.WriteLine("To High!");
+                } 
+                else if (i < winNum)
+                {
+                    Console.WriteLine("To Low!");
+                } 
+                else if (i == winNum)
+                {
+                    Console.WriteLine("You Win!");
+                    win = true;
+                }
+                
+                Console.WriteLine();
+            } while (win == false);
+            
+            Console.Write("Press any button to finish...");
+            Console.ReadKey(true);
+        }
+    }
+}
